af23fb6d533420da7b243e5638df543aa91a0daa
Dimantrie <tankerlord@mail.ru>
2021-11-21 21:29:27 +0300

    Revert "Create 1234.js"
    This reverts commit 04854f8552e87940f0427d85c76998e6e1de7d6a.

--- 1234.js
+++
@@ -1,7 +0,0 @@
-let ar = [],
-    arl = '';
-for (let i = 0, j = 0; i < sentence.length; i++) {
-    if (sentence[i] === ' ') { j++; continue; } else { arl[i] += sentence[i]; }
-    ar[j] += arl;
-}
-console.log(ar);
